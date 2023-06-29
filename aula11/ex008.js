1d04f6b5e1921cf3c4de9572c622330aedba4f74
ronistonqueiros <135164332+ronistonqueiros@users.noreply.github.com>
2023-06-29 08:17:00 -0300

    29

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if(vel > 60) { //condição Simples!
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO !`)
+}
+console.log(`Dirija sempre utilizando o cinto de segurança`)
