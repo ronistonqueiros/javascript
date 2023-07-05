88db1f99fed9017ac91faa80cb13c788aba232cc
ronistonqueiros <135164332+ronistonqueiros@users.noreply.github.com>
2023-07-05 12:52:23 -0300

    30

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,9 +1,19 @@
 var c = 1
-while (c <= 3) {
+do {
+    console.log(`Passo ${c}`)
+    c++ // c =c +1
+}while (c <=6)
+
+
+
+
+
+/*var c = 1
+while (c <= 6) {
     console.log(`Passo ${c}`)
     c++ // c =c +1
 }
-
+*/
 
 /*
 console.log('Tudo bem?')
